3f91893164a3ed8e7532f4dd93343909be4c84f8
putilovskaya <putilovskayaa@mail.ru>
2024-06-18 22:42:25 +0300

    initial app lms, create models Course and Lesson

---
+++ lms/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название курса")
+    preview = models.ImageField(upload_to='lms/course', **NULLABLE)
+    description = models.TextField(verbose_name="Описание курса", **NULLABLE)
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание урока", **NULLABLE)
+    preview = models.ImageField(upload_to='lms/lesson', **NULLABLE)
+    video_link = models.URLField(verbose_name="Ссылка на видео", **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return f'{self.title}'
