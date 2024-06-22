d1c1f6ba78e6a59e0f812f14995f4e0b6f12906b
putilovskaya <putilovskayaa@mail.ru>
2024-06-22 18:12:29 +0300

    add list of lessons to CourseSerializer as lessons

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,18 +4,19 @@ from rest_framework.serializers import ModelSerializer
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     count_lessons = SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True)
 
     def get_count_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = "__all__"
