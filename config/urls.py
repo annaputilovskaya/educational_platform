5a7285d293ee078f44beeea5572649b5da642b75
putilovskaya <putilovskayaa@mail.ru>
2024-06-22 14:06:20 +0300

    add media to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("lms/", include("lms.urls", namespace='lms')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
