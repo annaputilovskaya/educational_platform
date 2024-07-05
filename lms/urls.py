fb68666aa09abf09bfbc40b3c7b7c7f616832bdb
putilovskaya <putilovskayaa@mail.ru>
2024-07-05 21:43:40 +0300

    fix urls.py

--- lms/urls.py
+++ lms/urls.py
@@ -12,14 +12,14 @@ router = SimpleRouter()
 router.register("course", CourseViewSet)
 
 urlpatterns = [
-    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
+    path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
     path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson"),
-    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
     path(
-        "lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
+        "lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson-update"
     ),
     path(
-        "lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"
+        "lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson-delete"
     ),
     path("subscription/", SubscriptionApiView.as_view(), name="subscription"),
 ]
